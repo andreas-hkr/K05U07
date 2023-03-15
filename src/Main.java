6a68b00dc50010b8f2956212799bf69453666749
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 19:57:37 +0100

    More comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Beräkna summan av 1/1 - 1/2 + 1/3 - 1/4 + ....
+        // Fortsätt tills absolutbeloppet på termen är < 0.00001
 
 
     }
