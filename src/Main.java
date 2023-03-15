ace007edcf8c78d0033ca6e332ac71ce94c95949
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 20:04:01 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ public class Main {
         // Beräkna summan av 1/1 - 1/2 + 1/3 - 1/4 + ....
         // Fortsätt tills absolutbeloppet på termen är < 0.00001
 
+        String op = "+";
+        double sum = 0;
+        double x = 1;
 
+        while (Math.abs(1/x) >= 0.00001) {
+            if (op.equals("+")) {
+                sum += 1/x;
+                op = "-";
+            } else {
+                sum -= 1/x;
+                op = "+";
+            }
+            x++;
+        }
+        System.out.println("Summan: " + sum);
     }
 }
